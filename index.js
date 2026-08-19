3ff4a10a7c25e04222fcdae8024f735ad7e6430c
AhmadNawfalRamadhan <Ahmadnawfalramadhan@gmail.com>
2026-08-19 14:47:59 +0700

    Refactor database connection logic for improved clarity and maintainability

--- index.js
+++ index.js
@@ -1,66 +1,19 @@
-'use strict';
+const express = require("express");
+const connectDatabase = require("./config/db");
 
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
-const pg = require("pg");
-const process = require('process');
-const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.js')[env];
-const db = {};
+const app = express();
+const PORT = 3000;
 
-let sequelize;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-if (config.use_env_variable) {
-  const connectionUrl = process.env[config.use_env_variable];
+app.use("/api", require("./routes/api"));
 
-  const url = new URL(connectionUrl);
-
-  url.searchParams.delete("sslmode");
-
-  sequelize = new Sequelize(url.toString(), {
-    ...config,
-    dialect: "postgres",
-    dialectModule: pg,
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false
-      }
-    }
-  });
-} else {
-  sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
-    config
-  );
+async function startServer() {
+    await connectDatabase();
+    app.listen(PORT, () => {
+        console.log(`Server is running on http://localhost:${PORT}`);
+    });
 }
 
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (
-      file.indexOf('.') !== 0 &&
-      file !== basename &&
-      file.slice(-3) === '.js' &&
-      file.indexOf('.test.js') === -1
-    );
-  })
-  .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-    db[model.name] = model;
-  });
-
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
-
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
-
-module.exports = db;
+startServer();
